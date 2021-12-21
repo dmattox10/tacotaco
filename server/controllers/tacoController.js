232435d097d691a86ed975eb4db9de9a92859c36
Daniel Mattox <dmattox10@Daniels-Mac-mini.local>
2021-12-21 11:04:04 -0500

    pushing noDupes before I cut it again

--- server/controllers/tacoController.js
+++ server/controllers/tacoController.js
@@ -2,28 +2,62 @@ import mongoose from 'mongoose'
 import { status, errorOut, entry, operation } from '../lib/logging.js'
 import Entry from '../models/entry.js'
 
-let baseLayers, condiments, mixins, seasonings, shells, fullTacos = []
+let tacoGod = {}
 
 const prepare = async () => {
-    baseLayers = await Entry.find( {category: 'base_layers'} )
-    condiments = await Entry.find( {category: 'condiments'} ) 
-    mixins = await Entry.find( {category: 'mixins'} )
-    seasonings = await Entry.find( {category: 'seasonings'} )
-    shells = await Entry.find( {category: 'shells'} )
-    fullTacos = await Entry.find( {category: 'full_tacos'} )
+    tacoGod.baseLayers = await Entry.find( {category: 'base_layers'} ),
+    tacoGod.condiments = await Entry.find( {category: 'condiments'} ), 
+    tacoGod.mixins = await Entry.find( {category: 'mixins'} ),
+    tacoGod.seasonings = await Entry.find( {category: 'seasonings'} ),
+    tacoGod.shells = await Entry.find( {category: 'shells'} ),
+    tacoGod.fullTacos = await Entry.find( {category: 'full_tacos'} )
 }
 
+const pickRandom = (items) => {
+    const item = items[Math.floor(Math.random()*items.length)]
+    return item
+}
 
 const random = async (req, res) => {
-    res.status(200).json( { message: 'hi' } )
+    const base = pickRandom(tacoGod.baseLayers)
+    const condiment = pickRandom(tacoGod.condiments)
+    const mixin = pickRandom(tacoGod.mixins)
+    const seasoning = pickRandom(tacoGod.seasonings)
+    const shell = pickRandom(tacoGod.shells)
+    const taco = {
+        bases: [base],
+        condiments: [condiment],
+        mixins: [mixin],
+        seasonings: [seasoning],
+        shells: [shell]
+    }
+    res.status(200).json( { taco: taco } )
+}
+
+const noDupes = (items, output) => {
+    const item = items[Math.floor(Math.random()*items.length)]
+    if (output.indexOf(item) === -1) {
+        output.push(item)
+        return output
+    } else {
+        return noDupes(items, output)
+    }
 }
 
 const custom = async (req, res) => {
+    let taco = {}
+    for (const [key, value] of Object.entries(req.query)) {
+        while (taco[key].length -1 < value) {
+            taco[key].push()
+        }
+    }
+
     res.status(200).json( { message: 'hi' } )
 }
 
 const full = async (req, res) => {
-    res.status(200).json( { message: 'hi' } )
+    const taco = pickRandom(tacoGod.fullTacos)
+    res.status(200).json( { tacos: taco } )
 }
 
 prepare()
