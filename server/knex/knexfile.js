eae25e65bb4695e91e63163033991dbdf17f757f
Daniel Mattox <dmattox10@gmail.com>
2022-10-08 09:32:21 +0700

    Changes to knexfile because config options were not working

--- server/knex/knexfile.js
+++ server/knex/knexfile.js
@@ -1,19 +1,6 @@
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = require('../env.js')
 const path = require('path')
 
-const options = {
-  client: 'mysql2',
-  connection: {
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME
-  },
-  migrations: {
-    tableName: 'knex_migrations'
-  }
-}
-
 module.exports = {
 
   development: {
@@ -28,11 +15,29 @@ module.exports = {
   },
 
   staging: {
-    options
+    client: 'mysql2',
+    connection: {
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASS,
+      database: DB_NAME
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
 
   production: {
-    options
+    client: 'mysql2',
+    connection: {
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASS,
+      database: DB_NAME
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 
 }
