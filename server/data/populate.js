3c9cea5f3717136211d3fd382b67456625be5173
Daniel Mattox <dmattox10@gmail.com>
2022-10-09 08:57:31 +0700

    Trying absolute paths

--- server/data/populate.js
+++ server/data/populate.js
@@ -17,7 +17,7 @@ if (!Object.entries) {
 
 exports.populate = async () => {
   const finalEntries = []
-  const tree = await DirectoryTree('data/tacofancy')
+  const tree = await DirectoryTree('/data/tacofancy')
   const children = tree.children
   const output = {
     base_layers: [],
