5762bf02a3e5f0dff564fd586efbe9d9874a545d
Daniel Mattox <dmattox10@gmail.com>
2022-10-09 21:38:57 +0700

    removed returning from knex seed

--- server/knex/seeds/01_populate_entries_table.js
+++ server/knex/seeds/01_populate_entries_table.js
@@ -4,5 +4,5 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex('entries').del()
   const finalEntries = await populate()
-  await knex('entries').returning('id').insert(finalEntries)
+  await knex('entries').insert(finalEntries)
 }
