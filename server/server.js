4dca9b82e696a419e94a656d04f04b84b9f3db44
Daniel Mattox <dmattox10@gmail.com>
2022-10-10 11:07:51 +0700

    Style changes, responsive

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.use(urlencodedParser)
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.set('views', path.join(__dirname, '/views/'))
+app.set('/views', path.join(__dirname, '/views/'))
 app.use('/public', express.static(path.join(__dirname, '/public/')))
 
 app.use('/', async (req, res, next) => {
