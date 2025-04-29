d5f50a318f1e442d3ee05465fb3d02443721d689
Daniel Mattox <dmattox10@gmail.com>
2025-04-29 16:41:23 -0400

    changes to docker stuff

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
